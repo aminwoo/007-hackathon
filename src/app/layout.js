624e12f74d54f9fe148cc8f162aed29d3794e6e7
Ely Hawkins <44911472+Wheelsy@users.noreply.github.com>
2025-05-01 10:04:18 +1000

    new sounds system

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./animations.css";
+import SoundControlWrapper from "./components/SoundControlWrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <SoundControlWrapper />
         {children}
       </body>
     </html>
